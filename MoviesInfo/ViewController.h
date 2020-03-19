a178638510f63d6bd61af0df1e73992a6559d69a
iMac Arcsa Cesar <cesar.vargas@arcsa.com.mx>
2020-03-19 10:47:27 -0600

    Se inicia el proyecto
    Se crea un proyecto de Xcode en Obj C

---
+++ MoviesInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoviesInfo
+//
+//  Created by Cesar Roberto on 19/03/20.
+//  Copyright © 2020 Cesar Roberto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MoviesInfo/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  MoviesInfo
+//
+//  Created by Cesar Roberto on 19/03/20.
+//  Copyright © 2020 Cesar Roberto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
